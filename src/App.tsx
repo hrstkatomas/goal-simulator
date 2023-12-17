2f0a5dc530b069bebe04d58848a000cc93fc17cd
Tomas Hrstka <tomas.hrstka@livesport.eu>
2023-12-17 19:56:03 +0100

    installed react three fiber and tried demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef, useState } from "react";
+import { Canvas, ThreeElements, useFrame } from "@react-three/fiber";
+import { Mesh } from "three";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+function Box(props: ThreeElements["mesh"]) {
+	const meshRef = useRef<Mesh>(null!);
+	const [hovered, setHover] = useState(false);
+	const [active, setActive] = useState(false);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	useFrame((_state, delta) => (meshRef.current.rotation.x += delta));
+
+	return (
+		<mesh
+			{...props}
+			ref={meshRef}
+			scale={active ? 1.5 : 1}
+			onClick={() => setActive(!active)}
+			onPointerOver={() => setHover(true)}
+			onPointerOut={() => setHover(false)}
+		>
+			<boxGeometry args={[1, 1, 1]} />
+			<meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+		</mesh>
+	);
 }
 
-export default App
+function App() {
+	return (
+		<Canvas>
+			<ambientLight />
+			<pointLight position={[10, 10, 10]} />
+			<Box position={[-1.2, 0, 0]} />
+			<Box position={[1.2, 0, 0]} />
+		</Canvas>
+	);
+}
+
+export default App;
